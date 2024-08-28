6c07c463c4ed976df0b984aa9e44e032a9f7892b
desgurpreet <des.gurpreetsingh@gmail.com>
2024-08-28 12:12:03 +0530

    using array in useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 function App() {
   const [value, setValue] = useState(100);
   const [banned, setBanned] = useState({ name: "gurpreet", isBanned: false });
+  const [val, setVal] = useState([1, 2, 3, 4, 5, 6]);
   //value for value (use inside () of useState)
   //setValue give function to change the useState value
   //in react we cannot change value directly
@@ -24,6 +25,22 @@ function App() {
           className="p-2 bg-gray-400 text-white rounded"
         >
           Change
+        </button>
+      </div>
+      <div>
+        {val.map((item) => (
+          <h1>{item}</h1>
+        ))}
+        <button
+          onClick={() => 
+            setVal(() => 
+              val.filter((item, index) => 
+                index != val.length - 1
+              ))
+          }
+          className="p-2 bg-gray-400 text-white rounded bg-blue-500"
+        >
+          update
         </button>
       </div>
     </div>
