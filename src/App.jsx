314da4423c6278fa952171da5770f034b154917a
desgurpreet <des.gurpreetsingh@gmail.com>
2024-08-28 10:52:02 +0530

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-// import Card from "./Components/Card";
-import Sound from "./Components/Sound";
+import React, { useState } from "react";
 function App() {
+  const [value, setValue] = useState(100);
+  const [banned, setBanned] = useState({ name: "gurpreet", isBanned: false });
+  //value for value (use inside () of useState)
+  //setValue give function to change the useState value
+  //in react we cannot change value directly
   return (
-    <div>
-      {/* <Card></Card> */}
-      <Sound></Sound>
+    <div className="p-4">
+      <div>
+        <h1>{value}</h1>
+        <button
+          onClick={() => setValue(value == 100 ? 200 : 100)}
+          className="p-2 bg-gray-400 text-white rounded"
+        >
+          Increment
+        </button>
+      </div>
+      <div>
+        <h1>name: {banned.name}</h1>
+        <h1>isBanned: {banned.isBanned.toString()}</h1>
+        <button
+          onClick={() => setBanned({ ...banned, isBanned: !banned.isBanned })}
+          className="p-2 bg-gray-400 text-white rounded"
+        >
+          Change
+        </button>
+      </div>
     </div>
   );
 }
