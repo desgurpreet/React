d91017b6e3e0cacd20821c4ae96cf1c5bf7d5ec6
desgurpreet <des.gurpreetsingh@gmail.com>
2024-10-04 14:36:52 +0530

    createing  navbar using NAVLINK

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -6,13 +6,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-
+import Navbar from "./components/Navbar";
 function App() {
   //in Route path='/' represent localhost:3000 but in vite localhost:5173 hota hai
   // and element={<Home/>} localhost per home ko call lyi
   //like localhost:3000/home
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
