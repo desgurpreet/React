a687888b401af624f12342b59365c090964f3539
desgurpreet <des.gurpreetsingh@gmail.com>
2024-10-04 16:28:05 +0530

    go back button navigate

--- reactRouter/src/components/Contact.jsx
+++ reactRouter/src/components/Contact.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>welcome to my contact page</h1>
+      <button onClick={() => navigate(-1)}>Go Back</button>
     </div>
   );
 };
