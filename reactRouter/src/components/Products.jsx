25bffe85ffeaa7f8c216682b89cfab62b9169e28
desgurpreet <des.gurpreetsingh@gmail.com>
2024-11-06 15:48:40 +0530

    relative & absolute path

--- reactRouter/src/components/Products.jsx
+++ reactRouter/src/components/Products.jsx
@@ -8,6 +8,9 @@ const Products = () => {
         <h1>This is my product page</h1>
       </div>
       <nav style={{marginTop:20}}>
+        {/* inside Link [to="shirts"] is RELATIVE PATH
+        
+        IF we <Link to='/products/shirts'>Shirts</Link> is ABSOLUTE PATH */}
         <Link to="shirts">Shirts</Link>
         <Link to="jeans">Jeans</Link>
       </nav>
