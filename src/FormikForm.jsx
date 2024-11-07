e762c378b5750be1ee52540df5ac62efdbec9bb8
desgurpreet <des.gurpreetsingh@gmail.com>
2024-11-07 16:36:10 +0530

    touched ,blue property (formik)

--- src/FormikForm.jsx
+++ src/FormikForm.jsx
@@ -9,34 +9,50 @@ const FormikForm = () => {
   };
   //useFormik hook return a object using this obj
   //we can use (the complexities that is hold inside)
-  const formik = useFormik({
-    initialValues: formInitialValues,
-    validationSchema:FormSchema,
-    onSubmit: (values) => {
-      console.log(values);
-      console.log(values.name);
-      console.log(values.email);
-    },
-  });
+  // const formik = useFormik({
+  //destructuring as place of formik variable
+  const { handleSubmit, handleChange, handleBlur, touched, values, errors } =
+    useFormik({
+      initialValues: formInitialValues,
+      validationSchema: FormSchema,
+      onSubmit: (values) => {
+        console.log(values);
+        console.log(values.name);
+        console.log(values.email);
+      },
+    });
   return (
     <div>
       <h1>Formik Demo</h1>
-      <form onSubmit={formik.handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="">Enter Name: </label>
         <input
           type="text"
           name="name"
-          onChange={formik.handleChange}
-          value={formik.values.name}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.name}
         />
         <br />
-        <span style={{color:'red'}}>{formik.errors.name}</span>
+        {/* in simple way error sms <span style={{ color: "red" }}>{errors.name}</span> */}
+        {/* using touched formik for error sms  */}
+        {errors.name && touched.name ? (
+          <span style={{ color: "red" }}>{errors.name}</span>
+        ) : null}
         <br />
         <br />
         <label htmlFor="">Enter Email: </label>
-        <input type="text" name="email" onChange={formik.handleChange} />
+        <input
+          type="text"
+          name="email"
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
         <br />
-        <span style={{color:'red'}}>{formik.errors.email}</span>
+        {/* <span style={{ color: "red" }}>{errors.email}</span> */}
+        {errors.email && touched.email ? (
+          <span style={{ color: "red" }}>{errors.email}</span>
+        ) : null}
         <br />
         <br />
         <input type="submit" value="Submit" />
